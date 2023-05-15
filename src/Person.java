8b9f090a7ac9fefe8065a14be293760a0de50d36
Ismatulla Mansurov <47342870+sapoepsilon@users.noreply.github.com>
2023-05-15 11:39:44 -0600

    remove main class

--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,3 @@
 public class Person {
     // Copy paste your Person class here. Make sure class is class Person
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
 }
